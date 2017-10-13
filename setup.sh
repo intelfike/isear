a651a6c2b1dfbaec3eea2d0ebc1f0fea43b1dcfd
intelfike <intelfike@gmail.com>
2017-10-13 12:47:32 +0900

    setupを更新

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
+#!/bin/bash
 cd `dirname $0`
-cd ..
 sudo apt install -y nodejs nodejs-legacy
 sudo apt install -y npm
 sudo npm install -g typescript
 
 sudo npm install -g google-closure-compiler-js
-cd /usr/local/lib/node_modules/google-closure-compiler-js/
-sudo sed "s/ready(...arr)/ready.apply({}, arr)/" cmd.js | sudo tee cmd_tmp.js
+pushd /usr/local/lib/node_modules/google-closure-compiler-js/
+sudo sed "s/ready(...arr)/ready.apply(null, arr)/" cmd.js | sudo tee cmd_tmp.js
 sudo chmod 755 cmd_tmp.js
 sudo mv cmd.js cmd_backup.js
 sudo mv cmd_tmp.js cmd.js
-cd `dirname $0`
-cd ..
 
-npm install -D @types/chrome
-npm i --save-dev  @types/es6-promise
+popd
+npm install --save-dev @types/chrome
+npm install --save-dev @types/es6-promise
