5a94c629463353098612635777fd550d95533c22
intelfike <intelfike@gmail.com>
2018-01-02 22:23:12 +0900

    ver1.45: Firefox再起動時に右クリックメニューが消えるバグ修正

--- dev/background/ts/background.ts
+++ dev/background/ts/background.ts
@@ -57,32 +57,30 @@ async function highlighting(tabId:number){
 	await storageSet(saveNumPrefix+tabId, words_nums)
 }
 
-browser.runtime.onInstalled.addListener(()=>{
-	browser.contextMenus.create({
-		title: 'isear 検索ワードに追加',
-		type: "normal",
-		id: 'select',
-		contexts: ['selection']
-	})
-	browser.contextMenus.create({
-		 title: 'ハイライトのON/OFFを切り替える',
-		 type: "normal",
-		 id: 'toggle_highlight',
-		 contexts: ['browser_action']
-	})
-	browser.contextMenus.create({
-		title: 'ハイライトバー(右側)の表示/非表示を切り替える',
-		type: "normal",
-		id: 'toggle_bars',
-		contexts: ['browser_action']
-	})
-	browser.contextMenus.create({
-		title: '検索ワードをクリアする',
-		type: "normal",
-		id: 'clear',
-		contexts: ['browser_action']
-	})
-});
+browser.contextMenus.create({
+	title: 'isear 検索ワードに追加',
+	type: "normal",
+	id: 'select',
+	contexts: ['selection']
+})
+browser.contextMenus.create({
+	 title: 'ハイライトのON/OFFを切り替える',
+	 type: "normal",
+	 id: 'toggle_highlight',
+	 contexts: ['browser_action']
+})
+browser.contextMenus.create({
+	title: 'ハイライトバー(右側)の表示/非表示を切り替える',
+	type: "normal",
+	id: 'toggle_bars',
+	contexts: ['browser_action']
+})
+browser.contextMenus.create({
+	title: '検索ワードをクリアする',
+	type: "normal",
+	id: 'clear',
+	contexts: ['browser_action']
+})
 
 browser.contextMenus.onClicked.addListener(async function(itemData) {
 	switch(itemData.menuItemId){
