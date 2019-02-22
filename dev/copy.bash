d18492d17012877cd89776a3ea9e2cb0dca8122c
intelfike <intelfike@gmail.com>
2019-02-22 13:32:33 +0900

    ver1.93: ワードをたどる時のハイライトが表示されないバグを修正

--- dev/copy.bash
+++ dev/copy.bash
@@ -1,9 +1,11 @@
 echo '@copy manifest.json'
 cp manifest.json ../build/manifest.json
+cp style.css ../build/
 echo '@copy data'
 cp -r ../data ../build
 
 echo '@copy manifest.json for firefox'
 cp manifest_firefox.json ../firefox/build/manifest.json
+cp style.css ../firefox/build/
 echo '@copy data for firefox'
 cp -r ../data ../firefox/build
