832e9b0240f820092ecdc03fb4fae5ee805e1939
intelfike <intelfike@gmial.com>
2020-07-02 21:49:01 +0900

    ubuntu18.04向けにセットアップ用ファイルを修正

--- setup.bash
+++ setup.bash
@@ -1,16 +1,11 @@
 #!/bin/bash
 cd `dirname $0`
-sudo apt install -y nodejs nodejs-legacy
-sudo apt install -y npm
+sudo apt install -y nodejs
+sudo apt install -y nodejs-dev node-gyp libssl1.0-dev npm
 sudo npm install -g typescript
+sudo npm isntall -g npx
+sudo apt install closure-compiler
+npm install --save google-closure-compiler
 
-sudo npm install -g google-closure-compiler-js
-pushd /usr/local/lib/node_modules/google-closure-compiler-js/
-sudo sed "s/ready(...arr)/ready.apply(null, arr)/" cmd.js | sudo tee cmd_tmp.js
-sudo chmod 755 cmd_tmp.js
-sudo mv cmd.js cmd_backup.js
-sudo mv cmd_tmp.js cmd.js
-
-popd
 npm install --save-dev @types/chrome
 npm install --save-dev @types/es6-promise
